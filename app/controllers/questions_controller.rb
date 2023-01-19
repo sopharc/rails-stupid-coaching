90f7cf69df691b1c1f9d3375c4a1f22c82cd0a23
Sophie Archer <hello@sophiearcher.co.uk>
2023-01-19 21:16:23 +0000

    stupid coaching done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,5 +2,15 @@ class QuestionsController < ApplicationController
   def ask
   end
   def answer
+      @question = params[:question]
+      if @question.blank?
+        @answer = "Huh?"
+      elsif @question == "I'm going to work"
+       @answer = 'Great!'
+      elsif @question.include? '?'
+       @answer = 'Silly question, get dressed and go to work!'
+      else
+       @answer = "I don't care, get dressed and go to work!"
+      end
   end
 end
